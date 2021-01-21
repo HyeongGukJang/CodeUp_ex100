4638d967476761598194ab2e182927bf97a17372
= <gudrnr2004@naver.com>
2021-01-21 22:54:31 +0900

    .

---
+++ src/ex1003_1010/Code.java
@@ -0,0 +1,41 @@
+package ex1003_1010;
+
+public class Code {
+	public static void main(String[] args) {
+		new Code();
+	}
+
+	public Code() {
+//		ex1003();
+//		ex1004();
+//		ex1005();
+//		ex1006();
+//		ex1007();
+		ex1008();
+		ex1010();
+	}
+	private void ex1003() {
+		System.out.println("Hello\nworld");
+	}
+	private void ex1004() {
+		System.out.println("'Hello'");
+	}
+	private void ex1005() {
+		System.out.println("\"Hello World\"");
+	}
+	private void ex1006() {
+		System.out.println("\"!@#$%^&()*\"");
+	}
+	private void ex1007() {
+		System.out.println("\"C:\\Download\\hello.cpp\"");
+	}
+	private void ex1008() {
+		System.out.println("\u250C\u252C\u2510");
+		System.out.println("\u251C\u253C\u2524");
+		System.out.println("\u2514C\u2534C\u2518");
+	}
+	private void ex1010() {
+		int n = 3;
+		System.out.println(n);
+	}
+}
